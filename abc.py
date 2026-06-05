d98c62bb75e1b4c683b16ebc70ed9fd293be1177
ibrahimmalik0319-collab <ibrahimmalik0319@gmail.com>
2026-06-05 16:02:30 +0500

    Update abc.py

--- abc.py
+++ abc.py
@@ -273,7 +273,7 @@ div[data-testid="stSelectbox"] > label { display: none !important; }
 }
 .card-img-wrap img {
     width: 100%;
-    max-height: 300px;
+    max-height: 50px;
     object-fit: contain;
     display: block;
 }
